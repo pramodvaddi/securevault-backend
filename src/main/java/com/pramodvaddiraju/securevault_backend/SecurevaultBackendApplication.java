77907b1c08413554e2f60ee3496e804dba6fd187
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-14 12:07:17 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/securevault_backend/SecurevaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.securevault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurevaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurevaultBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramodvaddiraju/securevault_backend/SecurevaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.securevault_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurevaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
